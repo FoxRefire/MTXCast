3b9e33a8abe56d95d2e9f8b397c6fef5f74b5fb6
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2025-11-29 06:41:59 +0900

    open as window

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -9,3 +9,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         })
     }
 });
+
+if(chrome.windows){
+    chrome.action.onClicked.addListener(() => {
+        chrome.windows.create({
+            url: "popup/popup.html",
+            type: "popup",
+            width: 710,
+            height: 570
+        });
+    });
+} else {
+    chrome.action.onClicked.addListener(() => {
+        chrome.tabs.create({url: "popup/popup.html"})
+    })
+}
